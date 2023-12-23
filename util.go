6a6a229ce1d4125e9d1d9fcee6920b2b7d43dda2
Nyan <git@nyan.su>
2023-12-24 02:06:05 +0300

    rewrite uuid generator

--- util.go
+++ util.go
@@ -6,21 +6,7 @@ import (
 )
 
 func GenerateUUID() string {
-	runes := []rune("0123456789abcdef")
-
-	buf := make([]byte, 32)
-	rand.Read(buf)
-
-	result := make([]rune, 32)
-	for k, v := range buf {
-		result[k] = runes[int(v)%16]
-	}
-
-	p1 := result[0:8]
-	p2 := result[8:12]
-	p3 := result[12:16]
-	p4 := result[16:20]
-	p5 := result[20:32]
-
-	return fmt.Sprintf("%s-%s-%s-%s-%s", string(p1), string(p2), string(p3), string(p4), string(p5))
+	buf := make([]byte, 16)
+	 rand.Read(buf)
+	return fmt.Sprintf("%x-%x-%x-%x-%x", buf[0:4], buf[4:6], buf[6:8], buf[8:10], buf[10:])
 }
